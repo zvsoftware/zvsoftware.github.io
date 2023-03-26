9e3ff74e6a41fa3c30904923f19fea5ad84f47bc
Timothy Batt <jammincoderguy96@gmail.com>
2023-03-25 21:59:46 -0400

    updated site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zvsoftware.github.io',
+    site: 'https://zoarvalleysoftware.com',
 });
