dabd2b5f0f28253ce1278e240f74b8ef3e27691f
Timothy Batt <timothybatt3@gmail.com>
2023-05-08 12:46:08 -0400

    improved design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 export default function Nav(props) {
     return (
-        <nav className='flex gap-10 p-6 bg-white text-black items-center'>
+        <nav className='flex gap-10 p-6 bg-white text-black items-center relative z-50'>
                 
                 <a href="/" className='font-medium text-2xl flex items-center gap-4'>
                     <Logo className='w-12'/>
