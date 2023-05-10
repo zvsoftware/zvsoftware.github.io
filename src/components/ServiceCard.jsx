4cfe5158de1205a6f81e005bcad0821d00831291
Timothy Batt <timothybatt3@gmail.com>
2023-05-10 16:15:07 -0400

    Added about section

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,8 +1,10 @@
 export default function ServiceCard(props) {
     return (
-        <div className='p-4 grid text-center bg-[#0a0a0a] w-100% max-w-[20rem]'>
+        <div className='rounded p-4 grid place-items-center text-center bg-[#1c1c1c] w-100% max-w-[30rem] lg:max-w-[25rem]'>
             <h2 className='text-3xl font-medium mb-8'>{ props.title }</h2>
             <p className='text-gray-100'>{ props.children }</p>
+
+            <a href="/#contact" className='bg-primary btn w-fit block mt-6'>Get a quote</a>
         </div>
     );
 }
