000a33224e1dbf3090a83961d2edcf505ae48942
Timothy Batt <timothybatt3@gmail.com>
2023-05-10 21:00:14 -0400

    added blur effects

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -1,3 +1,3 @@
 export default function Blur(props) {
-    return <div className={`${ props.className } rounded-full aspect-square`} style={{ filter: `blur(${ props.blur || 100 }px)` }}></div>
+    return <div className={`${ props.className } rounded-full aspect-square absolute`} style={{ filter: `blur(${ props.blur || 100 }px)` }}></div>
 }
