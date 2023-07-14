224df9805fa356413a2ae759bcbf5d37906a99d1
Timothy Batt <timothybatt3@gmail.com>
2023-07-14 12:15:26 -0400

    made progress on reviews section

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,22 +1,26 @@
 export function Stars(props) {
     const stars = [];
-    const _class = props.className ? ' ' + props.className: '';
-    for (let i = 0; i < props.count; i++) {
+    
+    for (let i = 0; i < props.count; i++)
         stars.push(<img src='/img/star.svg' width={ 16 }/>);
-    }
+    
     return (
-        <div className={ 'flex gap-1' + _class }>
+        <div className='flex gap-1'>
             { stars }
         </div>
     );
 }
 
 export default function Review(props) {
+    const _class = props.className ? ' ' + props.className: '';
+
     return (
-        <div className='bg-slate-800 shadow p-4 rounded'>
-            <h3 className='align-middle'>{ props.author }<span className='text-sm text-gray-300'>{ props.company ? ' - ' + props.company : '' }</span></h3>
-            <Stars count={ props.rating } />
-            <p className='mt-4'>{ props.review }</p>  
+        <div className={'bg-slate-800 shadow p-8 rounded grid place-items-center'  + _class }>
+            <div>
+                <h3 className='align-middle'>{ props.author }<span className='text-sm text-gray-300'>{ props.company ? ' - ' + props.company : '' }</span></h3>
+                <Stars count={ props.rating } />
+                <p className='mt-4'>{ props.review }</p>  
+            </div>
         </div>
     );
 }
