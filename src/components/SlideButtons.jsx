49a406297c7ac7a73c5d1be6e31fb33baf66d293
Timothy Batt <timothybatt3@gmail.com>
2023-07-15 13:56:13 -0400

    slide indicators work for clicks, scrolls, and swipes

--- src/components/SlideButtons.jsx
+++ src/components/SlideButtons.jsx
@@ -13,12 +13,24 @@ export function SlideButton(props) {
         window.scrollTo(0, y);
     }
 
-    return <a style={{ backgroundColor: props.isActive ? 'white': '' }}  className={`bg-slate-500 w-3 aspect-square inline-block rounded-full`} onClick={ handleClick }></a>
+    return <a style={{ backgroundColor: props.isActive ? 'white': '' }}  className={`bg-slate-500 w-4 aspect-square inline-block rounded-full`} onClick={ handleClick }></a>
 }
 
 export default function SlideButtons(props) {
     const [activeSlide, setActiveSlide] = useState(0);
     const [slideButtons, setSlideButtons] = useState(null);
+
+    const slider = document.querySelector('.slider');
+    const slideWidth = slider.clientWidth
+
+    slider.addEventListener("scroll", e => {
+        // Calculate the current slide num based off of the scroll offset and the width of the slide.
+        const scrollOffset = e.target.scrollLeft;
+        if (scrollOffset % slideWidth === 0) {
+            const slideNum = scrollOffset / slideWidth;
+            setActiveSlide(slideNum);
+        }
+    })
 
     useEffect(() => {
         const btns = [];
