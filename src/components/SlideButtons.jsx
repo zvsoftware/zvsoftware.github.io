7a3d4e19cbaeebf86a8fcd226595ea913c860b65
Timothy Batt <timothybatt3@gmail.com>
2023-07-15 00:20:17 -0400

    slide indecators now change colors

--- src/components/SlideButtons.jsx
+++ src/components/SlideButtons.jsx
@@ -1,23 +1,46 @@
+import { useEffect, useState } from 'react';
+
 export function SlideButton(props) {
     function handleClick(e) {
+        props.onClick();
+
         e.preventDefault();
         e.stopPropagation();
+
 
         const y = window.scrollY;
         document.querySelector(`${ props.href }`).scrollIntoView();
         window.scrollTo(0, y);
     }
 
-    return <a { ...props } className='bg-slate-200 w-3 aspect-square inline-block rounded-full' onClick={ handleClick }></a>
+    return <a style={{ backgroundColor: props.isActive ? 'white': '' }}  className={`bg-slate-500 w-3 aspect-square inline-block rounded-full`} onClick={ handleClick }></a>
 }
 
-export default function SlideButtons() {
+export default function SlideButtons(props) {
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [slideButtons, setSlideButtons] = useState(null);
+
+    useEffect(() => {
+        const btns = [];
+        for (let i = 0; i < props.count; i++) {
+            const href = `#slide-${ i }`;
+            const isActive = `#slide-${ activeSlide }` === href;
+            btns.push(
+                <SlideButton 
+                    key={ i } 
+                    isActive={ isActive } 
+                    href={ href } 
+                    onClick={ () => setActiveSlide(i) }
+                />
+            );
+        }
+
+        setSlideButtons(btns);
+    }, [activeSlide]);
+
     return (
         <div className='flex gap-2'>
-            <SlideButton href="#slide-1"/>
-            <SlideButton href="#slide-2"/>
-            <SlideButton href="#slide-3"/>
-            <SlideButton href="#slide-4"/>
+            { slideButtons }            
         </div>
     );
 }
